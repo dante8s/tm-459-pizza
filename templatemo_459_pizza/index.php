6e5bdb73f929d211bf561651a33c9932e5494dba
Dmytro <dinadima346@gmail.com>
2025-03-11 23:53:26 +0100

    dop

--- templatemo_459_pizza/index.php
+++ templatemo_459_pizza/index.php
@@ -1,32 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
+<?php
+	include('partials\head.php');
 
-	<title>Pizza - free responsive website template</title>
-	<meta name="keywords" content="">
-	<meta name="description" content="">
-    <meta name="author" content="templatemo">
-    <!-- 
-    Pizza Template
-    http://www.templatemo.com/preview/templatemo_459_pizza
-    -->
-    
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<!-- bootstrap -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<!-- font-awesome -->
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<!-- custom -->
-	<link rel="stylesheet" href="css/templatemo-style.css">
-	<!-- google font -->
-	<link href='//fonts.googleapis.com/css?family=Signika:400,300,600,700' rel='stylesheet' type='text/css'>
-	<link href='//fonts.googleapis.com/css?family=Chewy' rel='stylesheet' type='text/css'>
-
-</head>
+?>
 <body id="home" data-spy="scroll" data-target=".navbar-collapse">
 
 	<!-- start navigation -->
@@ -209,25 +184,10 @@
 	<!-- end contact -->
 
 	<!-- start footer -->
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<p>Copyright &copy; 2084 Company Name</p>
-					<hr>
-					<ul class="social-icon">
-						<li><a href="#" class="fa fa-facebook"></a></li>
-						<li><a href="#" class="fa fa-twitter"></a></li>
-						<li><a href="#" class="fa fa-instagram"></a></li>
-						<li><a href="#" class="fa fa-pinterest"></a></li>
-						<li><a href="#" class="fa fa-google"></a></li>
-						<li><a href="#" class="fa fa-github"></a></li>
-						<li><a href="#" class="fa fa-apple"></a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</footer>
+	<?php
+	include('partials\footer.php');
+
+?>
 	<!-- end footer -->
 
 	<script src="js/jquery.js"></script>
---
+++ templatemo_459_pizza/partials/head.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<title>Pizza - free responsive website template</title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+    <meta name="author" content="templatemo">
+    <!-- 
+    Pizza Template
+    http://www.templatemo.com/preview/templatemo_459_pizza
+    -->
+    
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- font-awesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!-- custom -->
+	<link rel="stylesheet" href="css/templatemo-style.css">
+	<!-- google font -->
+	<link href='//fonts.googleapis.com/css?family=Signika:400,300,600,700' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Chewy' rel='stylesheet' type='text/css'>
+
+</head>
